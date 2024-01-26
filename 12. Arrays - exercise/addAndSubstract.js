1126b49b9caddfb0006ce73bd0d8114835b0f211
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-01-26 10:35:25 +0200

    task1

---
+++ 12. Arrays - exercise/addAndSubstract.js
@@ -0,0 +1,26 @@
+function addAndSubstract(array) {
+
+    let sumOriginal = 0;
+    let sumModified = 0;
+
+    for(i = 0; i < array.length; i++) {
+
+        currentNumber = array[i];
+        sumOriginal += currentNumber;
+
+        if(currentNumber % 2 === 0) {
+            array[i] += i;
+        } else {
+            array[i] -= i;
+        }
+        sumModified += array[i];
+    }
+
+    console.log(array);
+    console.log(sumOriginal);
+    console.log(sumModified);
+
+
+}
+
+addAndSubstract([5, 15, 23, 56, 35]);

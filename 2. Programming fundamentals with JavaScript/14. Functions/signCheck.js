0d742d9e09193b099d5c7de2af852c3996673cf8
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-02-01 17:39:36 +0200

    signCheck

---
+++ 2. Programming fundamentals with JavaScript/14. Functions/signCheck.js
@@ -0,0 +1,17 @@
+function signCheck (n1, n2, n3) {
+
+    let firstnums = n1 * n2;
+    let result = (firtnums, n3) => firtnums * n3;
+    
+    if (result(firstnums, n3) < 0) {
+        console.log('Negative');
+    } else console.log('Positive');
+
+}
+
+signCheck(-5,
+
+    1,
+    
+    1);
+

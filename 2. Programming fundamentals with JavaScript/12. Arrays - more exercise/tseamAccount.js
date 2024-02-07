26ddc4757e0db50754e3231b85031cf278d6bdab
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-02-07 17:51:19 +0200

    tseam account 90% working

--- 2. Programming fundamentals with JavaScript/12. Arrays - more exercise/tseamAccount.js
+++ 2. Programming fundamentals with JavaScript/12. Arrays - more exercise/tseamAccount.js
@@ -1,39 +1,41 @@
 function tseamAccount(array) {
     let currentGames = array.shift().split(' ');
-    // console.log(currentGames);
-    // console.log(array);
 
     for (let currCommand of array) {
-        let [command, game] = currCommand.split(' ');
-        switch (command) {
-            case 'Install':
-                currentGames.push(game)
-                break;
-            case 'Uninstall':
-                if (currentGames.includes(game)) {
-                    let index = currentGames.indexOf(game);
-                    currentGames.splice(index, 1);
-                }
-                break;
-            case 'Update':
-                if (currentGames.includes(game)) {
-                    let index = currentGames.indexOf(game);
-                    let store = currentGames.splice(index, 1);
-                    currentGames.push(store.join(''));
-                }
-                break;
-            case 'Expansion':
-                if (currentGames.includes(game)) {
-                    let index = currentGames.indexOf(game);
-                    let currValue = currentGames[index];
-                    currValue += ' ' + game;
-                }
-                break;
+        if (currCommand === 'Play!') {
+            break;
+        } else {
+            let [command, game] = currCommand.split(' ');
+            switch (command) {
+                case 'Install':
+                    currentGames.push(game)
+                    break;
+                case 'Uninstall':
+                    if (currentGames.includes(game)) {
+                        let index = currentGames.indexOf(game);
+                        currentGames.splice(index, 1);
+                    } break;
+                case 'Update':
+                    if (currentGames.includes(game)) {
+                        let index = currentGames.indexOf(game);
+                        let store = currentGames.splice(index, 1);
+                        currentGames.push(store[0]);
+                    } break;
+                case 'Expansion':
+                    let [gameToSetExpansion, expansion] = game.split('-');
+                    let expansionFormatted = gameToSetExpansion + ':' + expansion;
+                    if (currentGames.includes(gameToSetExpansion)) {
+                        let index = currentGames.indexOf(gameToSetExpansion);
+                        currentGames.splice(index + 1, 0, expansionFormatted);
+                    } break;
+            }
         }
     }
 
-console.log(currentGames);
+    console.log(currentGames.join(' '));
 
 }
 
-tseamAccount(['CS WoW Diablo', 'Install LoL', 'Uninstall WoW', 'Update Diablo', 'Expansion CS-Go', 'Play!']);
+
+tseamAccount(['CS WoW Diablo', 'Uninstall XCOM', 'Update PeshoGame', 'Update WoW', 'Expansion Civ-V', 'Play!']);
+// tseamAccount(['CS WoW Diablo', 'Install LoL', 'Uninstall WoW', 'Update Diablo', 'Expansion CS-Go', 'Play!']);

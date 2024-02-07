9d8bfb826be3429ffe4541c5a7e00c8b0355e174
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-02-07 14:35:51 +0200

    adsf

--- 2. Programming fundamentals with JavaScript/15. Functions - more exercise/printDna.js
+++ 2. Programming fundamentals with JavaScript/15. Functions - more exercise/printDna.js
@@ -4,13 +4,13 @@ function printDna(number) {
     let counter = 0;
   
     for (let i = 0; i < number; i++) {
-      if (i % 4 == 0) {
+      if (i % 4 === 0) {
         console.log(`**${model[counter % 10]}${model[(counter % 10) + 1]}**`);
-      } else if (i % 4 == 1) {
+      } else if (i % 4 === 1) {
         console.log(`*${model[counter % 10]}--${model[(counter % 10) + 1]}*`);
-      } else if (i % 4 == 2) {
+      } else if (i % 4 === 2) {
         console.log(`${model[counter % 10]}----${model[(counter % 10) + 1]}`);
-      } else if (i % 4 == 3) {
+      } else if (i % 4 === 3) {
         console.log(`*${model[counter % 10]}--${model[(counter % 10) + 1]}*`);
       }
       counter += 2;

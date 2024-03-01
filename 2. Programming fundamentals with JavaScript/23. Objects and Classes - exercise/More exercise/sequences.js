21f621cf7368f5a34548828a3d6a221bc00f8383
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-01 15:46:39 +0200

    shit

--- 2. Programming fundamentals with JavaScript/23. Objects and Classes - exercise/More exercise/sequences.js
+++ 2. Programming fundamentals with JavaScript/23. Objects and Classes - exercise/More exercise/sequences.js
@@ -3,43 +3,35 @@ function sequences(input) {
     let sortedArrays = [];
     let uniqueArrays = [];
     const arrayOfArrays = input.map(str => JSON.parse(str));
-    console.log(input);
-    console.log(arrayOfArrays);
 
     for (let current of arrayOfArrays) {
-        let currentSorted = [...current].sort((a, b) => b - a);
+        let currentSorted = current.sort((a, b) => b - a);
         sortedArrays.push(currentSorted);
     }
 
     for (let i = 0; i < sortedArrays.length; i++) {
-        let isDuplicate = false;
-        for (let j = i + 1; j < sortedArrays.length; j++) {
-            
-            if (JSON.stringify(sortedArrays[i]) === JSON.stringify(sortedArrays[j])) {
-                isDuplicate = true;
-                break;
-            }
-        }
-        if (!isDuplicate) {
-            uniqueArrays.push(sortedArrays[i]);
+        let stringified = JSON.stringify(sortedArrays[i]);
+
+        if (!uniqueArrays.includes(stringified)) {
+            uniqueArrays.push(stringified);
         }
     }
-    let sortedUniqueArrays = uniqueArrays.sort((a, b) => a.length - b.length);
+
+    let sortedUniqueArrays = uniqueArrays.map((a) => JSON.parse(a)).sort((a, b) => a.length - b.length);
     for (let current of sortedUniqueArrays) {
-        // console.log(`[${current.join(', ')}]`);
+        console.log(`[${current.join(', ')}]`);
     }
 }
 
 
-sequences(
-    ["[-3, -2, -1, 0, 1, 2, 3, 4]",
-        "[10, 1, -17, 0, 2, 13]",
-        "[4, -3, 3, -2, 2, -1, 1, 0]"]
-);
+sequences(["[7.14, 7.180, 7.339, 80.099]",
+    "[7.339, 80.0990, 7.140000, 7.18]",
+    "[7.33, 7.180, 7.14, 80.99]",
+    "[7.339, 7.180, 7.14, 80.099]",
+])
 
-// sequences(["[7.14, 7.180, 7.339, 80.099]",
-
-// "[7.339, 80.0990, 7.140000, 7.18]",
-
-// "[7.339, 7.180, 7.14, 80.099]",
-// "[7.33, 7.180, 7.14, 80.099]"])
+// sequences(
+//     ["[-3, -2, -1, 0, 1, 2, 3, 4]",
+//         "[10, 1, -17, 0, 2, 13]",
+//         "[4, -3, 3, -2, 2, -1, 1, 0]"]
+// );
--- 2. Programming fundamentals with JavaScript/23. Objects and Classes - exercise/More exercise/testSequences.js
+++ 2. Programming fundamentals with JavaScript/23. Objects and Classes - exercise/More exercise/testSequences.js
@@ -1,43 +1,29 @@
 function sequences(input) {
-
     let sortedArrays = [];
-    let uniqueArrays = [];
-    const arrayOfArrays = input.map(str => JSON.parse(str));
+    let uniqueArrays = new Set();
+
+    const arrayOfArrays = input.map(str => JSON.parse(str.replace(/'/g, '"')));
 
     for (let current of arrayOfArrays) {
-        let currentSorted = current.sort((a, b) => b - a);
-        sortedArrays.push(currentSorted);
+        let currentSorted = current.slice().sort((a, b) => b - a);
+        sortedArrays.push({ original: current, sorted: currentSorted });
     }
 
-    for (let i = 0; i < sortedArrays.length; i++) {
-        let isDuplicate = false;
-        for (let j = i + 1; j < sortedArrays.length; j++) {
-            
-            if (JSON.stringify(sortedArrays[i]) === JSON.stringify(sortedArrays[j])) {
-                isDuplicate = true;
-                break;
-            }
-        }
-        if (!isDuplicate) {
-            uniqueArrays.push(sortedArrays[i]);
-        }
+    for (let current of sortedArrays) {
+        uniqueArrays.add(JSON.stringify(current.sorted));
     }
-    let sortedUniqueArrays = uniqueArrays.sort((a, b) => a.length - b.length);
-    
+
+    let sortedUniqueArrays = [...uniqueArrays].map(str => JSON.parse(str)).sort((a, b) => a.length - b.length);
+
     for (let current of sortedUniqueArrays) {
         console.log(`[${current.join(', ')}]`);
     }
 }
 
-sequences(
-    ["[-3, -2, -1, 0, 1, 2, 3, 4]",
-        "[10, 1, -17, 0, 2, 13]",
-        "[4, -3, 3, -2, 2, -1, 1, 0]"]
-);
+sequences(["[7.14, 7.180, 7.339, 80.099]",
 
-// sequences(["[7.14, 7.180, 7.339, 80.099]",
+"[7.339, 80.0990, 7.140000, 7.18]",
 
-// "[7.339, 80.0990, 7.140000, 7.18]",
-
-// "[7.339, 7.180, 7.14, 80.099]",
-// "[7.33, 7.180, 7.14, 80.099]"])
+"[7.33, 7.180, 7.14, 80.99]",
+"[7.339, 7.180, 7.14, 80.099]",
+])

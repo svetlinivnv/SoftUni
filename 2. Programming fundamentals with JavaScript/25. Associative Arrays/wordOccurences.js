b778314f6bc71441d86d34cddc0a02bc4c1a273e
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-01 14:55:04 +0200

    word occurences

---
+++ 2. Programming fundamentals with JavaScript/25. Associative Arrays/wordOccurences.js
@@ -0,0 +1,24 @@
+function wordOccurences(input) {
+
+    let wordsList = {}
+
+    input.forEach(word => {
+        if (!wordsList.hasOwnProperty(word)) {
+            wordsList[word] = 1;
+        } else {
+            wordsList[word] += 1;
+        }
+    });
+
+    let sortedWordsList = Object.entries(wordsList).sort((a, b) => b[1] - a[1]);
+
+    sortedWordsList.forEach(word => {
+        
+        console.log(`${word[0]} -> ${word[1]} times`);
+
+    });
+}
+
+wordOccurences(["Here", "is", "the", "first", "sentence",
+                "Here", "is", "another", "sentence", "And",
+                "finally", "the", "third", "sentence"]);

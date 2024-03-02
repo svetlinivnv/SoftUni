29bcdf220217d2197d168f5ece10f9a8ab603d83
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-02 20:10:50 +0200

    fix

--- 2. Programming fundamentals with JavaScript/26. Associative Arrays - exercise/oddOccurences.js
+++ 2. Programming fundamentals with JavaScript/26. Associative Arrays - exercise/oddOccurences.js
@@ -1,35 +1,27 @@
 function oddOccurences(input) {
 
     let wordsArr = input.split(' ').map((a) => a.toLowerCase());
-    let uniqueArray = [...new Set(wordsArr)];
-    let wordsWithCount = {}
+    let unique = [];
+    let wordOccurences = {}
 
     wordsArr.forEach(word => {
-        if (!wordsWithCount.hasOwnProperty(word)) {
-            wordsWithCount[word] = 1;
+        if (!unique.includes(word)) {
+            unique.push(word);
+        }
+        if (!wordOccurences.hasOwnProperty(word)) {
+            wordOccurences[word] = 1;
         } else {
-            wordsWithCount[word] += 1;
+            wordOccurences[word] += 1;
         }
-    });
-
-    let valid = [];
-
-    Object.entries(wordsWithCount).forEach(element => {
-        let word = element[0];
-        let count = element[1];
-        if (count % 2 !== 0) {
-            valid.push(word);
-        }
-
     });
 
     let output = [];
-
-    for (let currWord of uniqueArray) {
-        if (valid.includes(currWord)) {
+    unique.forEach(currWord => {
+        if (wordOccurences[currWord] % 2 !== 0) {
             output.push(currWord);
         }
-    }
+    });
+
     console.log(output.join(' '));
 }
 

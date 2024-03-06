88f21de07380804388a73826d0dfbdbad1d86791
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-06 13:45:17 +0200

    garages

---
+++ 2. Programming fundamentals with JavaScript/26. Associative Arrays - exercise/More exercise/garage.js
@@ -0,0 +1,26 @@
+function garage (input) {
+
+    let garages = {};
+
+    input.forEach(item => {
+        let [garage, info] = item.split(' - ');
+        let carInfo = info;
+        if (!garages.hasOwnProperty(garage)) {
+            garages[garage] = [];
+            garages[garage].push(carInfo);
+        } else
+            garages[garage].push(carInfo);
+    });
+    for (let current in garages) {
+        console.log(`Garage № ${current}`);
+        garages[current].forEach(car => {
+            carFormetted = car.split(': ').join(' - ');
+            console.log(`--- ${carFormetted}`);
+        });
+    }
+}
+
+garage(['1 - color: blue, fuel type: diesel', 
+        '1 - color: red, manufacture: Audi', 
+        '2 - fuel type: petrol', 
+        '4 - color: dark blue, fuel type: diesel, manufacture: Fiat']);

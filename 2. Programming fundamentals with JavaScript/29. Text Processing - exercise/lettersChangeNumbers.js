ad748bde8cd0e53d664d7ebe9be66c31d20ba651
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-08 15:11:27 +0200

    fix

--- 2. Programming fundamentals with JavaScript/29. Text Processing - exercise/lettersChangeNumbers.js
+++ 2. Programming fundamentals with JavaScript/29. Text Processing - exercise/lettersChangeNumbers.js
@@ -1,12 +1,12 @@
 function lettersChangeNumbers(input) {
 
+    let arrOfInputs = input.split(' ').filter(str => str != '');
     let result = 0;
 
-    let arrOfInputs = input.split(' ');
     for (let current of arrOfInputs) {
         let firstLetter = current[0];
         let lastLetter = current[current.length - 1];
-        let number = current.substring(1, current.length - 1);
+        let number = Number(current.substring(1, current.length - 1));
         let alphabetPositionFirst = firstLetter.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
         let alphabetPositionLast = lastLetter.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
 
@@ -28,5 +28,5 @@ function lettersChangeNumbers(input) {
 }
 
 lettersChangeNumbers('A12b s17G');
-lettersChangeNumbers('P34562Z q2576f H456z');
-lettersChangeNumbers('a1A');
+// lettersChangeNumbers('P34562Z q2576f H456z');
+// lettersChangeNumbers('a1A');

a9ea51b2dc45105d5fd08ed7b84734458d9bc51f
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-08 14:52:34 +0200

    finaltask

---
+++ 2. Programming fundamentals with JavaScript/29. Text Processing - exercise/lettersChangeNumbers.js
@@ -0,0 +1,32 @@
+function lettersChangeNumbers(input) {
+
+    let result = 0;
+
+    let arrOfInputs = input.split(' ');
+    for (let current of arrOfInputs) {
+        let firstLetter = current[0];
+        let lastLetter = current[current.length - 1];
+        let number = current.substring(1, current.length - 1);
+        let alphabetPositionFirst = firstLetter.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+        let alphabetPositionLast = lastLetter.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+
+        if (firstLetter !== firstLetter.toLowerCase()) {
+            number /= alphabetPositionFirst;
+        } else {
+            number *= alphabetPositionFirst;
+        }
+
+        if (lastLetter !== lastLetter.toLowerCase()) {
+            number -= alphabetPositionLast;
+        } else {
+            number += alphabetPositionLast;
+        }
+
+        result += number
+    }
+    console.log(result.toFixed(2));
+}
+
+lettersChangeNumbers('A12b s17G');
+lettersChangeNumbers('P34562Z q2576f H456z');
+lettersChangeNumbers('a1A');

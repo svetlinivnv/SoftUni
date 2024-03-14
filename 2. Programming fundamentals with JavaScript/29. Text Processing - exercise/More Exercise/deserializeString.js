261e5cd8f3ada592378337ff1404adc4c4a496c9
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-14 11:40:09 +0200

    deserialize string

--- 2. Programming fundamentals with JavaScript/29. Text Processing - exercise/More Exercise/deserializeString.js
+++ 2. Programming fundamentals with JavaScript/29. Text Processing - exercise/More Exercise/deserializeString.js
@@ -1,16 +1,29 @@
 function deserializeString(input) {
+
+    let output = [];
+    let outputLength = 0;
+
     for (let element of input) {
         if (element !== 'end') {
-
-        let letter = element[0];
-        let indexes = element.substring(2).split('/');
-        console.log(letter);
-        console.log(indexes);
-
+            let indexes = element.substring(2).split('/');
+            outputLength += indexes.length;
         }
     }
+    while (output.length < outputLength) {
+        output.push(0);
+    }
+    for (let element of input) {
+        if (element !== 'end') {
+            let letter = element[0];
+            let indexes = element.substring(2).split('/');
+            indexes.forEach(index => {
+                output[+index] = letter;
+            });
+        }
+    }
+    console.log(output.join(''));
 }
 
 deserializeString(['a:0/2/4/6',
-                   'b:1/3/5',
-                   'end']);
+    'b:1/3/5',
+    'end']);

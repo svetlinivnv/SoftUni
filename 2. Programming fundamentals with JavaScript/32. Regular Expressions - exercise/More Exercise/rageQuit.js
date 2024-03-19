c78294280593645ec979d45f0ebc2a81b42a788b
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-19 09:39:02 +0200

    issue was that input comes as an array, which was not mentioned in task

--- 2. Programming fundamentals with JavaScript/32. Regular Expressions - exercise/More Exercise/rageQuit.js
+++ 2. Programming fundamentals with JavaScript/32. Regular Expressions - exercise/More Exercise/rageQuit.js
@@ -1,17 +1,15 @@
-// Solutions works perfectly in VS, but runtime error occurs in Judge !?!?
-
 function rageQuit(input) {
     const pattern = /\D+[0-9]+/g;
-    const matches = input.match(pattern);
     let result = '';
-        matches.forEach(matchWithNr => {
-            let match = matchWithNr.match(/[^0-9]+/)[0];
-            let xTimes = matchWithNr.match(/[0-9]+/)[0];
-            result += match.toUpperCase().repeat(xTimes);
-        });
+    const matches = input[0].match(pattern);
+    matches.forEach(matchWithNr => {
+        let match = matchWithNr.match(/[^0-9]+/)[0];
+        let xTimes = +matchWithNr.match(/[0-9]+/)[0];
+        result += match.toUpperCase().repeat(xTimes);
+    });
     const uniqueSymbols = new Set(result.split(''));
     console.log(`Unique symbols used: ${uniqueSymbols.size}\n${result}`);
 }
 
-// rageQuit('a3');
-rageQuit('aSd2&5s@1')
+// rageQuit(['a3']);
+rageQuit(['aSd2&5s@1'])

05a4ca62eae768caf50db9943de9678cb3fd12ed
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-25 10:55:13 +0200

    fix

--- 2. Programming fundamentals with JavaScript/35. Exam Preparation/fancyBarcode.js
+++ 2. Programming fundamentals with JavaScript/35. Exam Preparation/fancyBarcode.js
@@ -1,20 +1,19 @@
 function fancyBarcode(input) {
     input.shift();
-    
     const pattern = /(@#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)/;
     const digitsPattern = /[0-9]/g;
+
     for (let barcode of input) {
         if (pattern.test(barcode)) {
-            validBarcodeFound = true;
             let productGroup;
             let digits = barcode.match(digitsPattern);
             if (digits !== null) {
-                productGroup = digits.join('');
+                productGroup = digits.join("");
             } else {
-                productGroup = '00';
+                productGroup = "00";
             }
             console.log(`Product group: ${productGroup}`);
-        } else console.log('Invalid barcode');
+        } else console.log("Invalid barcode");
     }
 }
 
@@ -23,7 +22,8 @@ function fancyBarcode(input) {
 //             "@###Brea0D@###",
 //             "@##Che4s6E@##"]));
 
-fancyBarcode(["6",
+fancyBarcode([
+            "6",
             "@###Val1d1teM@###",
             "@#ValidIteM@#",
             "##InvaliDiteM##",

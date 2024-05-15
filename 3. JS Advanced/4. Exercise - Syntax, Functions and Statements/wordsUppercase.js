4a09fae62cfc5a868f00952547ddb668b282862a
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-15 12:52:08 +0300

    asdf

--- 3. JS Advanced/4. Exercise - Syntax, Functions and Statements/wordsUppercase.js
+++ 3. JS Advanced/4. Exercise - Syntax, Functions and Statements/wordsUppercase.js
@@ -2,6 +2,8 @@ function wordsUppercase(input) {
 
     let inputArr = input.toUpperCase().split(/\W+/);
     let lastElement = inputArr.length - 1;
+    let test = !!inputArr[lastElement];
+    console.log(test);
     if (!inputArr[lastElement]) {
         inputArr.pop();
     }

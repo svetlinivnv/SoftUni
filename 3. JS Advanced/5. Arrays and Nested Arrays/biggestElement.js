05e87f9e32cb1d92cf09fc3468667559380e190b
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-17 13:55:19 +0300

    tst

--- 3. JS Advanced/5. Arrays and Nested Arrays/biggestElement.js
+++ 3. JS Advanced/5. Arrays and Nested Arrays/biggestElement.js
@@ -1,9 +1,15 @@
-function biggestElement() {
+function biggestElement(input) {
 
-    
+    let biggestElement = Number.NEGATIVE_INFINITY;
 
-
-
+    for (current of input) {
+        for (element of current) {
+            if (element > biggestElement) {
+                biggestElement = element;
+            }
+        }
+    }
+    return biggestElement;
 }
 
-biggestElement();
+console.log(biggestElement([[20, 50, 10], [8, 33, 145]]));

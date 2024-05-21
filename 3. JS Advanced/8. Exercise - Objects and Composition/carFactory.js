c096d2e044e378e3f8fb91573eb2d7f285cd016a
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-21 15:15:55 +0300

    a

--- 3. JS Advanced/8. Exercise - Objects and Composition/carFactory.js
+++ 3. JS Advanced/8. Exercise - Objects and Composition/carFactory.js
@@ -1,36 +1,37 @@
 function carFactory(car) {
+    let result = {
+        model: car['model'],
+    };
 
-    let smallEngine = { power: 90, volume: 1800 };
-    let normalEngine = { power: 120, volume: 2400 };
-    let monsterEngine = { power: 200, volume: 3500 };
+    let engines = {
+        small: { power: 90, volume: 1800 },
+        normal: { power: 120, volume: 2400 },
+        monster: { power: 200, volume: 3500 },
+    }
 
     if (car.power < 120) {
-        car['engine'] = smallEngine;
+        result['engine'] = engines.small;
     } else if (car.power < 200) {
-        car['engine'] = normalEngine;
+        result['engine'] = engines.normal;
     } else {
-        car['engine'] = monsterEngine;
+        result['engine'] = engines.monster;
     }
 
-    delete car['power']
+    let carriage = {
+        type: car['carriage'],
+        color: car['color'],
+    }
 
-    let carriageType = car['carriage'];
-    let carriageColor = car['color'];
-    delete car['carriage'];
-    car['carriage'] = { type: carriageType, color: carriageColor };
-    delete car['color'];
+    result['carriage'] = carriage;
 
     let wheelsSize = car['wheelsize'];
-    if (wheelsSize % 2 === 0) {
-        wheelsSize = Math.floor(wheelsSize - 1);
-    }
+    wheelsSize = wheelsSize % 2 === 0 ? Math.floor(wheelsSize - 1) : wheelsSize;
 
-    car['wheels'] = [wheelsSize, wheelsSize, wheelsSize, wheelsSize];
-    delete car['wheelsize'];
+    result['wheels'] = new Array(4).fill(wheelsSize);
 
-    return car;
+    return result;
 }
 
-// const output = carFactory({ model: 'VW Golf II', power: 90, color: 'blue', carriage: 'hatchback', wheelsize: 14 });
-const output = carFactory({ model: 'Opel Vectra', power: 110, color: 'grey', carriage: 'coupe', wheelsize: 17 });
+const output = carFactory({ model: 'VW Golf II', power: 90, color: 'blue', carriage: 'hatchback', wheelsize: 14 });
+// const output = carFactory({ model: 'Opel Vectra', power: 110, color: 'grey', carriage: 'coupe', wheelsize: 17 });
 console.log(output);

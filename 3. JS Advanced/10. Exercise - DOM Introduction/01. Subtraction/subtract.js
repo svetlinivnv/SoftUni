ffbfbd6b4be3aca147b0cfbc61ad3640ed7acd68
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-23 16:48:04 +0300

    a

--- 3. JS Advanced/10. Exercise - DOM Introduction/01. Subtraction/subtract.js
+++ 3. JS Advanced/10. Exercise - DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    let n1 = Number(document.getElementById('firstNumber').value);
+    let n2 = Number(document.getElementById('secondNumber').value);
+    let result = n1 - n2;
+    document.getElementById('result').textContent = result;
 }
--- 3. JS Advanced/10. Exercise - DOM Introduction/02. Pascal or Camel Case/solution.js
+++ 3. JS Advanced/10. Exercise - DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,41 @@
 function solve() {
-  //TODO...
+    let input = document.getElementById('text').value;
+    let convention = document.getElementById('naming-convention').value;
+
+    
+    let wordsArr = input.split(' ').map((word) => word.toLowerCase());
+    let result = [];
+    
+    function camelCase() {
+        for (let i = 0; i < wordsArr.length; i++) {
+            let wordAsArr = wordsArr[i].split('');
+            if (i === 0) {
+                result.push(wordAsArr.join(''));
+            } else {
+                wordAsArr[0] = wordAsArr[0].toUpperCase();
+                result.push(wordAsArr.join(''));
+            }
+        }
+        let returnValue = result.join('');
+        return (returnValue);
+    }
+    
+    function pascalCase() {
+        for (let word of wordsArr) {
+            let wordAsArr = word.split('');
+            wordAsArr[0] = wordAsArr[0].toUpperCase();
+            result.push(wordAsArr.join(''));
+        }
+        let returnValue = result.join('');
+        return (returnValue);
+    }
+    
+    let output = 'Error!';
+    if (convention === 'Camel Case') {
+        output = camelCase();
+    } else if (convention === 'Pascal Case') {
+        output = pascalCase();
+    }
+    
+    document.getElementById('result').textContent = output;
 }

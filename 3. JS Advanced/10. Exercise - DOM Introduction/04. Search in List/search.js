99206406abedbbe11cb42004bfc991b42b032d1c
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-23 20:15:11 +0300

    a

--- 3. JS Advanced/10. Exercise - DOM Introduction/04. Search in List/search.js
+++ 3. JS Advanced/10. Exercise - DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,26 @@
 function search() {
-   // TODO
+
+   const searchedString = document.getElementById('searchText').value;
+   let liElements = document.querySelectorAll('ul *');
+   let matchesPrint = document.getElementById('result');
+   let matches = 0;
+
+   matches = 0;
+
+   for (city of liElements) {
+      city.style = '';
+   }
+
+   for (city of liElements) {
+      if (city.textContent.includes(searchedString) && searchedString !== '') {
+         city.style = 'font-weight: bold; text-decoration: underline';
+         matches++;
+      }
+   }
+
+   if (matches > 0) {
+      matchesPrint.textContent = `${matches} matches found`;
+   } else {
+      matchesPrint.textContent = '';
+   }
 }

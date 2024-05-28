da9977149f14b85b797f6f632f4df65795a0055b
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-28 17:26:37 +0300

    a

--- 3. JS Advanced/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ 3. JS Advanced/11. DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,27 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let rows = document.querySelectorAll('tbody td:nth-of-type(even)');
+    let input = document.querySelector('input[name="email"]');
+    let result = document.getElementById('result');
+
+    let searchedEmail = input.value;
+
+    for (let row of rows) {
+        let email = row.textContent;
+
+        if (email === searchedEmail) {
+            row.parentElement.remove();
+            result.textContent = 'Deleted.'
+            break;
+        } else {
+            result.textContent = 'Not found.'
+        }
+    }
+
+    input.value = '';
+
+    setTimeout(() => {
+        result.textContent = '';
+    }, 3000);
+
+    input.focus();
 }
--- 3. JS Advanced/11. DOM Manipulations and Events/03. Add-Delete/app.js
+++ 3. JS Advanced/11. DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    //TODO...
+    const inputEl = document.getElementById('newItemText');
+    const itemsUlEl = document.getElementById('items');
+
+    const liElement = document.createElement("li");
+    liElement.textContent = inputEl.value;
+
+    itemsUlEl.append(liElement);
+    
+    inputEl.value = '';
+    inputEl.focus();
 }

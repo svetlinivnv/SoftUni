f94f3b9c2bb4ebf7611981f984b2558d30f4116b
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-30 00:12:50 +0300

    a

--- 3. JS Advanced/12. Exercise - DOM Manipulations and Events/01. Sections/script.js
+++ 3. JS Advanced/12. Exercise - DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let contentElement = document.getElementById('content');
+
+   words.forEach(word => {
+      let divElement = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.style.display = 'none';
+      divElement.addEventListener('click', (event) => {
+         pElement.style.display = '';
+      })
+      pElement.textContent = word;
+      divElement.append(pElement);
+      contentElement.append(divElement);
+   });
 }

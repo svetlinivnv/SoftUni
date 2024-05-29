b1a539b160606fd958c90c650b2c84961c65a2a9
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-29 21:26:13 +0300

    a

--- 3. JS Advanced/11. DOM Manipulations and Events/03. Add-Delete/app.js
+++ 3. JS Advanced/11. DOM Manipulations and Events/03. Add-Delete/app.js
@@ -4,9 +4,15 @@ function addItem() {
 
     const liElement = document.createElement("li");
     liElement.textContent = inputEl.value;
+    const deleteBtn = document.createElement("a");
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
 
+    deleteBtn.addEventListener('click', (event) => liElement.remove());
+
+    liElement.append(deleteBtn);
     itemsUlEl.append(liElement);
-    
+
     inputEl.value = '';
     inputEl.focus();
 }
--- 3. JS Advanced/11. DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ 3. JS Advanced/11. DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
-}
+    let gradientElement = document.getElementById('gradient');
+    
+    gradientElement.addEventListener('mousemove', moveInElement);
+    gradientElement.addEventListener('mouseout', moveOutOfElement);
+ 
+    function moveInElement(event) {
+        let result = Math.floor(event.offsetX / (event.target.clientWidth - 1) * 100);
+        document.getElementById('result').textContent = `${result}%`;
+    }
+ 
+    function moveOutOfElement() {
+        document.getElementById('result').textContent = "";
+    }
+};

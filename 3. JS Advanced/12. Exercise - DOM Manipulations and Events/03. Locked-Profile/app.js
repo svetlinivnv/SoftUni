90302feea621a12aa35d0b983d686b31c9697113
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-30 14:53:47 +0300

    a

--- 3. JS Advanced/12. Exercise - DOM Manipulations and Events/03. Locked-Profile/app.js
+++ 3. JS Advanced/12. Exercise - DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,34 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const users = document.querySelectorAll('div .profile');
+
+    users.forEach(user => {
+
+        const lock = user.querySelector('input[value="lock"');
+        const unlock = user.querySelector('input[value="unlock"');
+
+        const hiddenFields = user.querySelector('div[id$="HiddenFields"]');
+        const showMore = user.querySelector('button');
+
+        //Handle radio buttons:
+        lock.addEventListener('click', (event) => {
+            if (!lock.checked) {
+                unlock.checked = false;
+                lock.checked = true;
+            }
+        });
+        unlock.addEventListener('click', (event) => {
+            if (!unlock.checked) {
+                lock.checked = false;
+                unlock.checked = true;
+            }
+        });
+
+        //Handle show more:
+        showMore.addEventListener('click', (event) => {
+            if (!lock.checked) {
+                hiddenFields.style.display = hiddenFields.style.display === 'block' ? 'none' : 'block';
+                showMore.textContent = showMore.textContent === 'Show more' ? 'Hide it' : 'Show more';
+            }
+        });
+    });
 }

526c1b24f191ecd67cd0b532fa0e06f351f2428a
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-30 15:50:36 +0300

    a

--- 3. JS Advanced/12. Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ 3. JS Advanced/12. Exercise - DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
+
+    const messages = document.querySelectorAll('textarea');
+    const inputMessageEl = messages[0];
+    const receivedMessageEl = messages[1];
+
+    const buttons = document.querySelectorAll('button');
+    const encodeBtnEl = buttons[0];
+    const decodeBtnEl = buttons[1];
     
+    function messageEncoder() {
+        let encodedMessage = '';
+        let message = inputMessageEl.value.split('');
+        message.forEach(char => {
+            let charAscii = char.charCodeAt(0) + 1;
+            encodedMessage += String.fromCharCode(charAscii);
+        });
+        receivedMessageEl.value = encodedMessage;
+        inputMessageEl.value = '';
+        decodeBtnEl.disabled = false;
+    }
+    
+    function messageDecoder() {
+        let decodedMessage = '';
+        let message = receivedMessageEl.value.split('');
+        message.forEach(char => {
+            let charAscii = char.charCodeAt(0) - 1;
+            decodedMessage += String.fromCharCode(charAscii);
+        });
+        receivedMessageEl.value = decodedMessage;
+        decodeBtnEl.disabled = true;
+    }
+    encodeBtnEl.addEventListener('click', messageEncoder);
+    decodeBtnEl.addEventListener('click', messageDecoder);
 }

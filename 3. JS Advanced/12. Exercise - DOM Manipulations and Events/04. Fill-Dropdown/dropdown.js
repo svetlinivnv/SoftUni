f78c04645d8c22e08f59fac3eb768150f1ae5748
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-05-30 15:04:35 +0300

    a

--- 3. JS Advanced/12. Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ 3. JS Advanced/12. Exercise - DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    let textEl = document.querySelector('#newItemText');
+    let valueEl = document.querySelector('#newItemValue');
+    let dropdownEl = document.querySelector('select');
+
+    let text = textEl.value;
+    let value = valueEl.value;
+
+    let optionEl = document.createElement('option');
+    optionEl.textContent = text;
+    optionEl.value = value;
+
+    dropdownEl.append(optionEl);
+
+    textEl.value = '';
+    valueEl.value = '';
+    textEl.focus();
 }

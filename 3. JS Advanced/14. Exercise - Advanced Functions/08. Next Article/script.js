b2e683144f6e07e6dbfd0d7caf59c813784c710c
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-06-03 21:24:38 +0300

    a

--- 3. JS Advanced/14. Exercise - Advanced Functions/08. Next Article/script.js
+++ 3. JS Advanced/14. Exercise - Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const contentEl = document.querySelector('#content');
+    let state = 0;
+
+    return function showNext() {
+        let articleEl = document.createElement('article');
+        if (state < articles.length) {
+            let article = articles[state];
+            articleEl.textContent = article;
+            contentEl.append(articleEl);
+            state++
+        }
+    }
 }

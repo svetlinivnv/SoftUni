03a78a0c32890451bc5d6e8c5aab672bcd5e8bbe
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-06-06 01:11:30 +0300

    a

---
+++ 3. JS Advanced/15. Unit testing and Error handling/checkForSymetry/test/checkForSymetry.test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { isSymmetric } from '../checkForSymetry';
+
+describe('Check for symetry', () => {
+
+    it('takes an array as an argument', () => {
+        // expect(arr.)
+    });
+
+
+
+});

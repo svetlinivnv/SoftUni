2644717994489499b268fd340e162f6b704a091f
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-06-06 18:49:40 +0300

    a

---
+++ 3. JS Advanced/15. Unit testing and Error handling/sumOfNumbers/test/sumNumbers.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+
+import { sum } from '../sumNumbers.js';
+
+describe('Sum numbers', () => {
+
+    it('sums single number', () => {
+        expect(sum([1])).to.equal(1);
+    });
+
+    it('sums multiple numbers', () => {
+        expect(sum([1, 1])).to.equal(2);
+    });
+
+    it('sums different numbers', () => {
+        expect(sum([2, 3, 4])).to.equal(9);
+    });
+});

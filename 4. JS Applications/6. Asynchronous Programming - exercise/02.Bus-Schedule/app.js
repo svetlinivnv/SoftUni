d0a8b633dbebb09d51f6ee473cad95ff78836bd3
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-07-01 02:20:07 +0300

    a

--- 4. JS Applications/6. Asynchronous Programming - exercise/02.Bus-Schedule/app.js
+++ 4. JS Applications/6. Asynchronous Programming - exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,49 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    
+    let currentStopId = 'depot';
+    let currentStopName = '';
+    let nextStopId = '';
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoBox = document.querySelector('.info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    function updateInfoBox(message) {
+        infoBox.textContent = message;
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function fetchStop(stopId) {
+        try {
+            const response = await fetch(BASE_URL + stopId);
+            if (!response.ok) {
+                throw new Error('Stop not found');
+            }
+            return await response.json();
+        } catch (error) {
+            updateInfoBox('Error');
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+    }
+
+    async function depart() {
+        departBtn.disabled = true;
+        const stopInfo = await fetchStop(currentStopId);
+        if (stopInfo) {
+            currentStopName = stopInfo.name;
+            nextStopId = stopInfo.next;
+            updateInfoBox(`Next stop ${currentStopName}`);
+            arriveBtn.disabled = false;
+            currentStopId = nextStopId;
+        }
+    }
+    
+    async function arrive() {
+        updateInfoBox(`Arriving at ${currentStopName}`);
+        currentStopId = nextStopId;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {

8023cfc8224c7e90f85f512f00f889dd3483c5da
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-07-01 13:52:28 +0300

    a

--- 4. JS Applications/6. Asynchronous Programming - exercise/04.Locked-Profile/app.js
+++ 4. JS Applications/6. Asynchronous Programming - exercise/04.Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const USERS_URL = 'http://localhost:3030/jsonstore/advanced/profiles';
+
+    async function getUsers() {
+        const response = await fetch(USERS_URL);
+        const data = await response.json();
+
+
+    }
+
+    function createProfile(username, email, age) {
+        const hrElement = docment.createElement('hr');
+
+        const emailLabel = document.createElement('label');
+        emailLabel.textContent = 'Email:';
+
+        const emailInput = document.createElement('input');
+        
+
+    }
 }
